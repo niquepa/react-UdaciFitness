23edb5c34c9b4f8d4715538199c400286147863d
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-11-17 17:42:48 -0500

    - color red - eslint

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -47,27 +47,27 @@ export default class AddEntry extends Component {
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()} />
         {Object.keys(metaInfo).map((key) => {
-          const { getIcon, type, ...rest } = metaInfo[key];
-          const value = this.state[key];
+         const { getIcon, type, ...rest } = metaInfo[key];
+         const value = this.state[key];
 
-          return (
-            <View key={key}>
-              {getIcon()}
-              {type === 'slider'
-                ? <UdaciSlider
-                  value={value}
-                  onChange={value => this.slide(key, value)}
-                  {...rest}
-                />
-                : <UdaciSteppers
-                  value={value}
-                  onIncrement={() => this.increment(key)}
-                  onDecrement={() => this.decrement(key)}
-                  {...rest}
-                />}
-            </View>
-          );
-        })}
+         return (
+           <View key={key}>
+             {getIcon()}
+             {type === 'slider'
+         ? <UdaciSlider
+           value={value}
+           onChange={value => this.slide(key, value)}
+           {...rest}
+         />
+         : <UdaciSteppers
+           value={value}
+           onIncrement={() => this.increment(key)}
+           onDecrement={() => this.decrement(key)}
+           {...rest}
+         />}
+           </View>
+         );
+         })}
       </View>
     );
   }
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { white } from './colors';
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from 'react-native-vector-icons';
+import { red } from './colors';
 
 export function getMetricMetaInfo(metric) {
   const info = {
@@ -16,7 +16,7 @@ export function getMetricMetaInfo(metric) {
           <View>
             <MaterialIcons
               name="directions-run"
-              color={white}
+              color={red}
               size={35}
             />
           </View>
@@ -34,7 +34,7 @@ export function getMetricMetaInfo(metric) {
           <View>
             <MaterialCommunityIcons
               name="bike"
-              color={white}
+              color={red}
               size={32}
             />
           </View>
@@ -52,7 +52,7 @@ export function getMetricMetaInfo(metric) {
           <View>
             <MaterialCommunityIcons
               name="swim"
-              color={white}
+              color={red}
               size={35}
             />
           </View>
@@ -70,7 +70,7 @@ export function getMetricMetaInfo(metric) {
           <View>
             <FontAwesome
               name="bed"
-              color={white}
+              color={red}
               size={30}
             />
           </View>
@@ -88,7 +88,7 @@ export function getMetricMetaInfo(metric) {
           <View>
             <MaterialCommunityIcons
               name="food"
-              color={white}
+              color={red}
               size={35}
             />
           </View>
