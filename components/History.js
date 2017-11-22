33ffed7e8175bf188ace8c1fb68cbc12b6070cb9
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-11-22 09:01:19 -0500

    - Loading - MetricCard

--- components/History.js
+++ components/History.js
@@ -7,8 +7,13 @@ import { fetchCalendarResults } from '../utils/api';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
 import { white } from '../utils/colors';
 import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
+import { AppLoading } from 'expo';
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
   componentDidMount() {
     const { dispatch } = this.props;
 
@@ -35,7 +40,7 @@ class History extends Component {
         : <TouchableOpacity
           onPress={() => console.log('Pressed!')}
         >
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard date={formattedDate} metrics={metrics} />
         </TouchableOpacity>}
     </View>
   )
@@ -51,6 +56,10 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props;
+    const { ready } = this.state;
+    if (ready === false) {
+      return <AppLoading />;
+    }
     return (
       <UdaciFitnessCalendar
         items={entries}
